d38cd94fdc603578a85a0a585ef38a8caf77e1e2
emirhan.bayatli <emirhan.bayatli99@icloud.com>
2025-10-14 21:38:46 +0300

    Implement post listing and detail views with loading state; add List and Loading components

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,3 +1,29 @@
+"use client";
+import Loading from "@/app/components/loading";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function Post() {
-  return <>Post detail</>;
+  const params = useParams();
+  const postId = params.id;
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/posts/${postId}`)
+      .then((res) => res.json())
+      .then((data) => setPost(data));
+    setLoading(false);
+  }, [postId]);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loading />
+      </div>
+    );
+  }
+
+  console.log(post);
+  return <>Post detail {postId}</>;
 }
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import List from "../components/list";
+import Image from "next/image";
+import Loading from "../components/loading";
 type Post = {
   id: number;
   title: string;
@@ -20,15 +23,39 @@ export default function Posts() {
   }, []);
 
   console.log(posts);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loading />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Posts List</h1>
-      <span className="loading loading-dots loading-xl"></span>
-      <ul>
+    <div className="min-h-screen py-10 px-6">
+      <h1 className="text-4xl font-bold text-center mb-10">Post List</h1>
+      <ul className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
         {posts.map((post) => (
-          <Link key={post.id} href={`/posts/${post.id}`}>
-            <li key={post.id}>{post.title}</li>
-          </Link>
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+              <div className="bg-white shadow-md rounded-2xl overflow-hidden hover:shadow-lg transition-all duration-300 hover:-translate-y-1 ">
+                <div className="flex flex-col items-center p-6 relative">
+                  <img
+                    src={`https://randomuser.me/api/portraits/women/${post.id}.jpg`}
+                    alt="User profile"
+                    className="rounded-full mb-4 "
+                  />
+                  <h2 className="text-lg font-semibold text-gray-800 text-center mb-2 line-clamp-1">
+                    {post.title}
+                  </h2>
+                  <p className="text-sm text-gray-500 text-center line-clamp-2">
+                    {post.body}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          </li>
         ))}
       </ul>
     </div>
